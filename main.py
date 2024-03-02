218fa5fc6faf9b570a2fadb53a9954078d4d8f06
NibhrantV <136268322+NibhrantV@users.noreply.github.com>
2024-03-02 17:30:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if 'responses' not in st.session_state:
 if 'requests' not in st.session_state:
     st.session_state['requests'] = []
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key="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")
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key="")
 
 if 'buffer_memory' not in st.session_state:
     st.session_state['buffer_memory'] = ConversationBufferWindowMemory(k=3, return_messages=True)
